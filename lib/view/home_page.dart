0fb179968ac5ff656f2f11835053978bd4bf3776
PRATANGGA <pratangga31@gmail.com>
2024-12-11 17:28:57 +0700

    change home page

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -14,7 +14,6 @@ class HomePage extends StatelessWidget {
       body: FutureBuilder<List<ArticleModel>>(
         future: ApiService().fetchArticles(),
         builder: (context, snapshot) {
-          print(snapshot.data);
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
@@ -23,38 +22,57 @@ class HomePage extends StatelessWidget {
             final articles = snapshot.data!;
 
             return ListView.builder(
-              itemCount: articles.length, // Safely handle null data
+              itemCount: articles.length,
               itemBuilder: (context, index) {
                 final article = articles[index];
-                return ListTile(
-                  leading: article.urlToImage != null
-                      ? ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Container(
-                            // height: double.infinity
-                            width: MediaQuery.of(context).size.width / 3,
-                            child: Image.network(
-                              article.urlToImage!,
-                              width: double.infinity,
-                              height: double.infinity,
-                              fit: BoxFit.cover,
+                return Card(
+                  margin: const EdgeInsets.all(12.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(12)),
+                        child: article.urlToImage != null
+                            ? Image.network(
+                                article.urlToImage!,
+                                width: double.infinity,
+                                height:
+                                    200, // Ubah tinggi gambar sesuai kebutuhan
+                                fit: BoxFit.cover,
+                              )
+                            : Image.network(
+                                'https://images.unsplash.com/photo-1594322436404-5a0526db4d13?q=80&w=1129&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                                width: double.infinity,
+                                height: 200,
+                                fit: BoxFit.cover,
+                              ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              article.title ?? 'No Title',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        )
-                      : ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Container(
-                            width: MediaQuery.of(context).size.width / 3,
-                            child: Image.network(
-                              'https://images.unsplash.com/photo-1594322436404-5a0526db4d13?q=80&w=1129&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                              width: 100,
-                              height: 100,
-                              fit: BoxFit.cover,
+                            const SizedBox(height: 8),
+                            Text(
+                              article.description ?? 'No Description',
+                              style: const TextStyle(fontSize: 14),
                             ),
-                          ),
+                          ],
                         ),
-                  title: Text(article.title ?? 'No Title'),
-                  subtitle: Text(article.description ?? 'No Description'),
+                      ),
+                    ],
+                  ),
                 );
               },
             );
